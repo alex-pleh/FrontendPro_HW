5fa247cdc3e40481af667ad7ecc6e41f259f4f2f
Alexey <aleksejpleh@gmail.com>
2021-05-11 18:36:49 +0300

    Completed home work 2.

--- index.js
+++ index.js
@@ -14,3 +14,65 @@
 #   это не обязательно должен быть символ операции (+, -, *, /, %) может быть и текст. 
 # - использовать if else или switch case для вызова необходимой функции. 
 */
+//1)
+let arr = [undefined, null];
+arr.push(prompt("String", 'Hello'));
+arr.push(+prompt("Number", 100));
+arr.push(confirm("True(OK) or false(Отмена)"));
+arr.push(Symbol(prompt("Symbol", 'Id')));
+arr.push(BigInt(prompt("BigInt", 100000000000000000000000n)));
+arr.push({position: prompt("Position", "Manager") , salary: +prompt("Salary", 1000), remotely: Boolean(confirm("Remotely True(OK) or false(Отмена)"))});
+
+console.log(arr);
+
+// 2)
+
+let firstNum;
+let secondNum;
+do {
+    firstNum = +prompt("Please enter first number");
+    console.log(firstNum);
+    if(isNaN(firstNum)){
+        alert("You enter not a number, please try again");
+        console.log("You enter not a number, please try again");
+    }
+}
+while (isNaN(firstNum));
+
+const typeOperation = prompt("Please enter which operation do you want");
+console.log(typeOperation);
+
+do {
+    secondNum = +prompt("Please enter second number");
+    console.log(secondNum);
+    if(isNaN(secondNum)){
+        alert("You enter not a number, please try again");
+        console.log("You enter not a number, please try again");
+    }
+}
+while (isNaN(secondNum));
+
+function calc(firstNum, secondNum, typeOperation) {
+    switch (typeOperation) {
+        case '+':
+            return firstNum + secondNum;
+            break;
+        case '-':
+            return firstNum - secondNum;
+            break;
+        case '*':
+            return firstNum * secondNum;
+            break;
+        case '/':
+            return firstNum / secondNum;
+            break;
+        case '%':
+            return firstNum % secondNum;
+            break;
+        default:
+            return "You enter incorrect operation"
+    }
+}
+
+alert("Result: " + calc(firstNum, secondNum, typeOperation));
+console.log(("Result: "  + calc(firstNum, secondNum, typeOperation)));
